1d2243384d141a2370a87deb2c86e5c4b162c9f3
AlianBlank <wangfj11@foxmail.com>
2024-04-10 18:00:47 +0800

    [增加]1. 增加防裁剪标记

--- Runtime/GameFrameXGameAnalyticsCroppingHelper.cs
+++ Runtime/GameFrameXGameAnalyticsCroppingHelper.cs
@@ -1,10 +1,12 @@
-
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace GameFrameX.GameAnalytics.Runtime
 {
+    [Preserve]
     internal class GameFrameXGameAnalyticsCroppingHelper : MonoBehaviour
     {
+        [Preserve]
         private void Start()
         {
             _ = typeof(GameAnalyticsManager);
